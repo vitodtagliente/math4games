cef3d2ab08a839698bd68b947fb323e2c847fe25
Vito Domenico Tagliente <hackjoker@hotmail.it>
2017-12-01 20:22:35 +0100

    changes

---
+++ testlinux/test/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math4games/math4games.h>
+
+using namespace math4games;
+
+int main(int argc, char **argv) {
+    
+    vector2 v;
+    
+    
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
