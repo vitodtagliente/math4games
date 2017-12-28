7ccd5a018ea4c552ad6af8f4f9bc67a0435b328b
Vito Domenico Tagliente <hackjoker@hotmail.it>
2017-12-28 18:30:30 +0100

    matrix 3 and 4

--- include/math4games/matrix.h
+++ include/math4games/matrix.h
@@ -56,7 +56,7 @@ namespace math4games
 		}
 
 		T determinant() {
-			return T{}; // TODO
+			return T{}; // TODO for n x m matrix
 		}
 
 		static T determinant(matrix<n, m, T> M) {
@@ -92,7 +92,7 @@ namespace math4games
 		}
 
 		matrix<n, m, T> adjugate() {
-			return *this; // TODO
+			return *this; // TODO for n x m matrix
 		}
 
 		static matrix<n, m, T> adjugate(const matrix<n, m, T>& M) {
@@ -240,6 +240,119 @@ namespace math4games
 	template<typename T> const matrix<2, 2, T> tmatrix2<T>::zero = tmatrix2<T>(0.0, 0.0, 0.0, 0.0);
 	template<typename T> const matrix<2, 2, T> tmatrix2<T>::identity = tmatrix2<T>(1.0, 0.0, 0.0, 1.0);
 
+	template<typename T>
+	struct tmatrix3 : public matrix<3, 3, T>
+	{
+		tmatrix3() : matrix<3, 3, T>() {}
+
+		tmatrix3(
+			const T& a00, const T& a10, const T& a20,
+			const T& a01, const T& a11, const T& a21,
+			const T& a02, const T& a12, const T& a22
+		) {
+			data[0][0] = a00; data[0][1] = a10; data[0][2] = a20;
+			data[1][0] = a01; data[1][1] = a11; data[1][2] = a21;
+			data[2][0] = a02; data[2][1] = a12; data[2][2] = a22;
+		}
+		tmatrix3(const matrix<3, 3, T>& m) {
+			for (int i = 0; i < rows; i++)
+				for (int j = 0; j < columns; j++)
+					data[i][j] = m.data[i][j];
+		}
+		tmatrix3(const T& value) : matrix<3, 3, T>(value) {}
+		tmatrix3(const std::initializer_list<T> args) : matrix<3, 3, T>(args) {}
+
+		T determinant() {
+			return (data[0][0] * data[1][1] * data[2][2]) -
+				(data[0][1] * data[1][2] * data[2][0]) -
+				(data[0][2] * data[1][0] * data[2][1]);
+		}
+
+		tmatrix3 adjugate() {
+			return tmatrix3(); // TODO
+		}
+
+		tmatrix3<T>& operator= (const matrix<3, 3, T>& m) {
+			for (int i = 0; i < rows; i++)
+				for (int j = 0; j < columns; j++)
+					data[i][j] = m.data[i][j];
+			return *this;
+		}
+
+		static const matrix<3, 3, T> zero;
+		static const matrix<3, 3, T> identity;
+	};
+
+	template<typename T> const matrix<3, 3, T> tmatrix3<T>::zero = tmatrix3<T>(0.0);
+	template<typename T> const matrix<3, 3, T> tmatrix3<T>::identity = tmatrix3<T>(
+		1.0, 0.0, 0.0, 
+		0.0, 1.0, 0.0,
+		0.0, 0.0, 1.0
+	);
+
+	template<typename T>
+	struct tmatrix4 : public matrix<4, 4, T>
+	{
+		tmatrix4() : matrix<4, 4, T>() {}
+
+		tmatrix4(
+			const T& a00, const T& a10, const T& a20, const T& a30,
+			const T& a01, const T& a11, const T& a21, const T& a31,
+			const T& a02, const T& a12, const T& a22, const T& a32,
+			const T& a03, const T& a13, const T& a23, const T& a33
+		) {
+			data[0][0] = a00; data[0][1] = a10; data[0][2] = a20; data[0][3] = a30;
+			data[1][0] = a01; data[1][1] = a11; data[1][2] = a21; data[1][3] = a31;
+			data[2][0] = a02; data[2][1] = a12; data[2][2] = a22; data[2][3] = a32;
+			data[3][0] = a03; data[3][1] = a13; data[3][2] = a23; data[3][3] = a33;
+		}
+		tmatrix4(const matrix<4, 4, T>& m) {
+			for (int i = 0; i < rows; i++)
+				for (int j = 0; j < columns; j++)
+					data[i][j] = m.data[i][j];
+		}
+		tmatrix4(const T& value) : matrix<4, 4, T>(value) {}
+		tmatrix4(const std::initializer_list<T> args) : matrix<4, 4, T>(args) {}
+
+		T determinant() {
+			return T{}; // TODO
+		}
+
+		tmatrix4 adjugate() {
+			return tmatrix4(); // TODO
+		}
+
+		tmatrix4<T>& operator= (const matrix<4, 4, T>& m) {
+			for (int i = 0; i < rows; i++)
+				for (int j = 0; j < columns; j++)
+					data[i][j] = m.data[i][j];
+			return *this;
+		}
+
+		static const matrix<4, 4, T> zero;
+		static const matrix<4, 4, T> identity;
+	};
+
+	template<typename T> const matrix<4, 4, T> tmatrix4<T>::zero = tmatrix4<T>(0.0);
+	template<typename T> const matrix<4, 4, T> tmatrix4<T>::identity = tmatrix4<T>(
+		1.0, 0.0, 0.0, 0.0,
+		0.0, 1.0, 0.0, 0.0,
+		0.0, 0.0, 1.0, 0.0,
+		0.0, 0.0, 0.0, 1.0
+	);
+
 	typedef tmatrix2<float> matrix2;
+	typedef tmatrix3<float> matrix3;
+	typedef tmatrix4<float> matrix4;
 	typedef matrix2 m2;
+	typedef matrix3 m3;
+	typedef matrix4 m4;
+
+	typedef tmatrix2<double> dmatrix2;
+	typedef tmatrix3<double> dmatrix3;
+	typedef tmatrix4<double> dmatrix4;
+
+	typedef tmatrix2<int> imatrix2;
+	typedef tmatrix3<int> imatrix3;
+	typedef tmatrix4<int> imatrix4;
 };
